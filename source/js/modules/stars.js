6cecb90302ae1b3986151c4275e303b82c2bfba9
ANNAstasiyaBelskaya <belskayanastasiya@yandex.ru>
2025-04-25 20:56:29 +0400

    implement hero, tours, training sections

---
+++ source/js/modules/stars.js
@@ -0,0 +1,37 @@
+const createStars = (filled) => {
+  const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+  svg.setAttribute('width', '15');
+  svg.setAttribute('height', '13');
+
+  const use = document.createElementNS('http://www.w3.org/2000/svg', 'use');
+  use.setAttribute('href', '/__spritemap#sprite-star-fill-none');
+
+
+  if (filled) {
+    use.setAttribute('href', '/__spritemap#sprite-star-fill-white');
+  }
+
+  svg.appendChild(use);
+  return svg;
+};
+
+const getRating = () => {
+  document.addEventListener('DOMContentLoaded', () => {
+    const starsContainer = document.querySelectorAll('.card__info-list-item');
+    starsContainer.forEach((item) => {
+      const ratingSpan = item.querySelector('[data-rating]');
+
+      if(ratingSpan) {
+        const ratingValue = ratingSpan.dataset.rating;
+        ratingSpan.innerHTML = '';
+
+        for (let i = 0; i < 5; i++) {
+          const star = createStars(i < ratingValue);
+          ratingSpan.appendChild(star);
+        }
+      }
+    });
+  });
+};
+
+export { getRating };
---
+++ source/js/modules/trainers-slider.js
@@ -0,0 +1,37 @@
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
+
+const initTrainersSwiper = () => {
+  const swiper = new Swiper('.training__slider', {
+    modules: [ Navigation ],
+    navigation: {
+      nextEl: '.training__slider-control-button.swiper-button-next',
+      prevEl: '.training__slider-control-button.swiper-button-prev',
+    },
+    direction: 'horizontal',
+    loop: false,
+    grabCursor: true,
+    allowTouchMove: true,
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        initialSlide: 2,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+        initialSlide: 0,
+      },
+      1440: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+        initialSlide: 0,
+      },
+    }
+  });
+
+  return swiper;
+};
+
+export { initTrainersSwiper };
